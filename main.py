c27e0c3f4815e7d69c39fcaca48d39bd53675099
Akhil Movva <akhilmovva6852@gmail.com>
2021-10-13 21:57:29 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from item import Item
+# from phone import Phone
+
+item1 = Item("MyItem", 750, 6)
+
+item1.send_email()
+
+# item1.apply_increment(0.2)
+# item1.apply_discount()
+# print(item1.price)
+
+# item1.name = "OtherItem"
+# print(item1.name)
+
+# phone1 = Phone("jscPhonev10", 500, 5)
+# phone1.broken_phones = 1
+# phone2 = Phone("jscPhonev20", 700, 5, 1)
+#
+# print(Item.all)
+# print(Phone.all)
+
+# print(Item.is_integer(7))
+
+# item1 = Item("Phone", 100, 1)
+# item2 = Item("Laptop", 1000, 3)
+# item3 = Item("Cable", 10, 5)
+# item4 = Item("Mouse", 50, 5)
+# item5 = Item("Keyboard", 75, 5)
+
+# for instance in Item.all:
+#     print(instance.name)
+
+# print(Item.all)
+
+# item1.apply_discount()
+# print(item1.price)
+#
+# item2.pay_rate = 0.7
+# item2.apply_discount()
+# print(item2.price)
+
+# print(Item.__dict__)  # All the attributes for Class level
+# print(item1.__dict__)  # All the attributes for instance level
+
+# print(item1.calculate_total_price())
